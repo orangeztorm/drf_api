b364ec32e8992fca4f68a61bcc98798a295d7f15
orangeztorm <taiwokenny45@gmail.com>
2021-02-19 22:41:39 +0100

    understanding django mixins1

--- core/views.py
+++ core/views.py
@@ -2,32 +2,42 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 # third party import
+from rest_framework import mixins
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from .serializers import PostSerializer
 from .models import Post
+from rest_framework import generics
 
 
 # Create your views here.
 
-class TestView(APIView):
-    permission_classes = (IsAuthenticated,)
+class PostView(mixins.ListModelMixin, generics.GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
 
     def get(self, request, *args, **kwargs):
-        qs = Post.objects.all()
-        post = qs.first()
-        # serializer = PostSerializer(qs, many=True)
-        serializer = PostSerializer(post)
+
         return Response(serializer.data)
 
-    def post(self, request, *args, **kwargs):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+# class TestView(APIView):
+#     permission_classes = (IsAuthenticated,)
+#
+#     def get(self, request, *args, **kwargs):
+#         qs = Post.objects.all()
+#         post = qs.first()
+#         # serializer = PostSerializer(qs, many=True)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+#
+#     def post(self, request, *args, **kwargs):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
 
 # def test(request):
 #     data = {'name':'taiwo', 'age': 23 }
