1dfcd421d316cfa12a4f72ffb8b8df6d5973970a
orangeztorm <taiwokenny45@gmail.com>
2021-02-19 22:13:03 +0100

    understanding django auth token

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 
 # third party import
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from .serializers import PostSerializer
 from .models import Post
@@ -11,6 +12,8 @@ from .models import Post
 # Create your views here.
 
 class TestView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request, *args, **kwargs):
         qs = Post.objects.all()
         post = qs.first()
--- drf_api/settings.py
+++ drf_api/settings.py
@@ -32,14 +32,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
-    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
+    'rest_framework',
+    'rest_auth',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
